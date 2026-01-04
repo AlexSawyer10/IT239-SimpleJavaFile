226a84585d4ac19e61780a531c3a4608d2e3be41
Alex <alexvsawyer@gmail.com>
2026-01-03 22:25:42 -0500

    Fixed syntax error

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
     public static void main(String[] args) 
     {
-        System.out.prln("Hello, world! Alex Sawyer changed this");
+        System.out.println("fixed");
     }
 }
